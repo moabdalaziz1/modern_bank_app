d8084d05ae6746a08d1a3cf18ff5d563d2335382
mohamedabdelaziz <moabdalaziz6116@gmail.com>
2023-03-19 18:38:56 +0200

    z-index Added To The Sidebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -52,7 +52,7 @@ const Navbar = () => {
         <div
           className={`${toggle ? 'flex' : 'hidden'}
             p-6 bg-black-gradient absolute top-20 right-0 
-            mx-4 my-2 min-w-[140px] rounded-xl sidebar`}
+            mx-4 my-2 min-w-[140px] rounded-xl sidebar z-20`}
         >
         <ul 
           className="list-none flex flex-col justify-end items-center flex-1"
